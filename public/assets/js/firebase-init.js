6100237140b471aead342a3233d76423a16e533f
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-02-09 16:04:09 +0700

    test-3 firebase

--- public/assets/js/firebase-init.js
+++ public/assets/js/firebase-init.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: "AIzaSyBKIH6Xfr3Ix-JpMG0hgRqQteI44vVQq8s",
 	authDomain: "identifymeapps.firebaseapp.com",
